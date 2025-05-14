9fe0d09812e90e22e151833820c2349a59a88e0b
Lucas <lucas.baptista@live.com>
2025-05-14 20:38:47 -0300

    Scaffold inicial finalizado

--- SenaiSystem/Program.cs
+++ SenaiSystem/Program.cs
@@ -1,5 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy(
+            name: "minhasOrigens",
+            policy =>
+            {
+                //TODO: Alterar link para o Frontend
+                policy.WithOrigins("http://localhost:5500");
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            }
+        );
+    });
+
 var app = builder.Build();
+
+app.UseCors("minhasOrigens");
+
+app.MapControllers();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
 
 app.Run();
