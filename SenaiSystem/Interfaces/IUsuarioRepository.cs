3e1b195d83ca0402b8b7bb62a0de7e2bf31b248e
Lucas <lucas.baptista@live.com>
2025-05-15 21:36:57 -0300

    Removidos arquivos

--- SenaiSystem/Interfaces/IUsuarioRepository.cs
+++ SenaiSystem/Interfaces/IUsuarioRepository.cs
@@ -5,6 +5,7 @@ namespace SenaiSystem.Interface
     public interface IUsuarioRepository 
     {
         List<Usuario> ListarTodos();
+        Usuario BuscarPorEmailSenha(string email, string senha);
         void Cadastrar(Usuario usuario);
         Usuario? Atualizar(int id, Usuario usuario);    
         Usuario? Deletar(int id);
