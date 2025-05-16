04a8725a2d84d4abd76eed66548cde0381f58e5f
m4thi4ss <silvamathiasj@gmail.com>
2025-05-15 21:52:47 -0300

    Escondendo String de conexao com o banco de dados

--- SenaiSystem/Context/SenaiSystemContext.cs
+++ SenaiSystem/Context/SenaiSystemContext.cs
@@ -11,9 +11,11 @@ public partial class SenaiSystemContext : DbContext
     {
     }
 
-    public SenaiSystemContext(DbContextOptions<SenaiSystemContext> options)
+    private IConfiguration _configuration;
+    public SenaiSystemContext(DbContextOptions<SenaiSystemContext> options, IConfiguration confing)
         : base(options)
-    {
+    { 
+        _configuration = confing;   
     }
 
     public virtual DbSet<Categoria> Categoria { get; set; }
@@ -27,9 +29,12 @@ public partial class SenaiSystemContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=NOTE46-S28\\SQLEXPRESS;Initial Catalog=SenaiSystem;User id=sa;Password=Senai@134;TrustServerCertificate=true;");
+        { 
+        var con = _configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(con);
 
+        }
+        
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categoria>(entity =>
